be88a5f19a1c5d9b81da1ab307403f370415c075
0xDon <22967202+thedonmon@users.noreply.github.com>
2023-05-21 16:59:38 -0400

    corsss

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const latest = require("./api/latest");
-
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json({ extended: false }));
 
 app.use("/api/latest", latest);
