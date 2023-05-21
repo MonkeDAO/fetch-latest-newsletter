d72738f15d03de6be78d8caa7031b00c5aaf18de
0xDon <22967202+thedonmon@users.noreply.github.com>
2023-05-21 16:33:02 -0400

    try again

--- api/latest.js
+++ api/latest.js
@@ -7,13 +7,34 @@ const fetchLatestNewsletter = require("../fetcher");
  * @return newsletter description | empty object.
  */
 router.get("/", async (req, res) => {
-    try {
-     const latestNewsletter = await fetchLatestNewsletter()
-      res.json(latestNewsletter);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).send("Server error");
-    }
+  try {
+    const latestNewsletter = await fetchLatestNewsletter()
+     res.json(latestNewsletter);
+   } catch (error) {
+     console.error(error);
+     return res.status(500).send("Server error");
+   }
   });
   
   module.exports = router;
+
+// const app = require('express')();
+// const fetchLatestNewsletter = require("../fetcher");
+
+// app.get('/api', (req, res) => {
+//   try {
+//       return fetchLatestNewsletter().then(res => {
+//           return res.status(200).json(res);
+//       })
+//     } catch (error) {
+//       console.error(error);
+//       return res.status(500).send("Server error");
+//     }
+// });
+
+// app.get('/api/item/:slug', (req, res) => {
+//   const { slug } = req.params;
+//   res.end(`Item: ${slug}`);
+// });
+
+// module.exports = app;
